7d7c1f82bedf3e8e708405c28e86e4370b5b71e2
JemarJones <jemarkjones@gmail.com>
2015-09-19 23:03:00 -0400

    Added populating categories via array

--- js/categories.js
+++ js/categories.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-	resizeItems(getOptimalRows());
+  init();
+  createBindings();
+});
+var init = function(){
+  // $.get("getcategoriesendpoint",function(data){
+    var cats = ['books','movies','shows','bars','food','books','movies','shows','bars','food','books','movies','shows','bars','food','books','movies','shows','bars','food'];//data.cats;
+    for (var i = 0; i < cats.length; i++) {
+        $('#container').append('<div class="item i' + ((i % 4) + 1) + '">' + cats[i] + '</div>');
+    }
+    resizeItems(getOptimalRows());
+
     var $container = $('#container').packery();
     // init
     $container.packery({
@@ -13,29 +23,30 @@ $(document).ready(function(){
       // bind Draggabilly events to Packery
       $container.packery( 'bindDraggabillyEvents', draggie );
     });
-    $(window).resize(function(){
-		resizeItems(getOptimalRows());
-    });
-});
-
+  // });
+};
+var createBindings = function(){
+  $(window).resize(function(){
+    resizeItems(getOptimalRows());
+  });
+};
 var resizeItems = function(perRow){
-	console.log("resizing: " + perRow);
-	var size = ($('#container').width() - 10*perRow)/perRow;
-	$('.item').width(size);
-	$('.item').height(size);
+  var size = ($('#container').width() - 10*perRow)/perRow;
+  $('.item').width(size);
+  $('.item').height(size);
 };
 
 var getOptimalRows = function(){
-	var contWidth = $('#container').width();
-	if (contWidth > 1068){
-		return 6;
-	}else if(contWidth <= 1068 && contWidth > 980){
-		return 5;
-	}else if(contWidth <= 980 && contWidth > 620){
-		return 4;
-	}else if (contWidth <= 620 && contWidth > 460){
-		return 3;
-	}else if (contWidth <= 460 && contWidth > 300){
-		return 2;
-	}
+  var contWidth = $('#container').width();
+  if (contWidth > 1068){
+    return 6;
+  }else if(contWidth <= 1068 && contWidth > 980){
+    return 5;
+  }else if(contWidth <= 980 && contWidth > 620){
+    return 4;
+  }else if (contWidth <= 620 && contWidth > 460){
+    return 3;
+  }else if (contWidth <= 460 && contWidth > 300){
+    return 2;
+  }
 };
